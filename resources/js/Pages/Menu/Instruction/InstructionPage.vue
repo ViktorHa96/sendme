e6c0cae62abbe8f3cccbec3bb1cbd29f393b8496
ViktorHa96 <119804202+ViktorHa96@users.noreply.github.com>
2023-09-12 16:34:20 +0000

    how to section stylovanie , pridanie ikoniek

--- resources/js/Pages/Menu/Instruction/InstructionPage.vue
+++ resources/js/Pages/Menu/Instruction/InstructionPage.vue
@@ -2,26 +2,82 @@
     <Head title="Ako na to" />
    <AppLayout>
     <div class="specify-font howto-background flex flex-col text-center ">
-    <div class="flex flex-col h-full gap-8">
-        <div class="w-1/5 flex flex-col place-items-center">
-            <div class="w-14 h-14 rounded-full bg-cyan-400"></div>
-            <h1>Klikni na vytvoriť poštu</h1>
+    <div class="flex flex-col h-full gap-8 mb-10">
+        <div class="w-1/5 flex flex-col place-items-center border-b-2 border-r-2 border-gray-500 py-4 px-3 rounded-lg">
+                <div class="p-8 rounded-full bg-cyan-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-click" width="46" height="46" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M3 12l3 0"></path>
+                        <path d="M12 3l0 3"></path>
+                        <path d="M7.8 7.8l-2.2 -2.2"></path>
+                        <path d="M16.2 7.8l2.2 -2.2"></path>
+                        <path d="M7.8 16.2l-2.2 2.2"></path>
+                        <path d="M12 12l9 3l-4 2l-2 4l-3 -9"></path>
+                    </svg>
+                </div>
+                <h1 class="font-bold text-2xl border-b-4 border-[#ff948f] leading-tight rounded-full mb-2">1.</h1>
+                <h1>Klikni na vytvoriť poštu</h1>
+            </div>
+        <div class="w-1/5 flex flex-col place-items-center border-b-2 border-r-2 border-gray-500 py-4 px-3 rounded-lg">
+            <div class="p-8 rounded-full bg-cyan-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-book-upload" width="46" height="46" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M14 20h-8a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12v5"></path>
+                    <path d="M11 16h-5a2 2 0 0 0 -2 2"></path>
+                    <path d="M15 16l3 -3l3 3"></path>
+                    <path d="M18 13v9"></path>
+                </svg>
+            </div>
+            <h1 class="font-bold text-2xl border-b-4 border-[#ff948f] leading-tight rounded-full mb-2">2.</h1>
+            <h1>Vyber si zo stoviek návrhov dizajnov alebo nahraj svoj.</h1>
         </div>
-        <div class="w-1/5 flex flex-col place-items-center">
-            <div class="w-14 h-14 rounded-full bg-cyan-400"></div>
-            <h1>Vyber si z našej galérie</h1>
+        <div class="w-1/5 flex flex-col place-items-center border-b-2 border-r-2 border-gray-500 py-4 px-3 rounded-lg">
+            <div class="p-8 rounded-full bg-cyan-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-writing" width="46" height="46" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M20 17v-12c0 -1.121 -.879 -2 -2 -2s-2 .879 -2 2v12l2 2l2 -2z"></path>
+                    <path d="M16 7h4"></path>
+                    <path d="M18 19h-13a2 2 0 1 1 0 -4h4a2 2 0 1 0 0 -4h-3"></path>
+                </svg>
+            </div>
+            <h1 class="font-bold text-2xl border-b-4 border-[#ff948f] leading-tight rounded-full mb-2">3.</h1>
+            <h1>Vyber si zo stoviek textových veršov alebo si vytvor svoj.</h1>
         </div>
-        <div class="w-1/5 flex flex-col place-items-center">
-            <div class="w-14 h-14 rounded-full bg-cyan-400"></div>
-            <h1>Vytvor text alebo si vyber z návrhov</h1>
+        <div class="w-1/5 flex flex-col place-items-center border-b-2 border-r-2 border-gray-500 py-4 px-3 rounded-lg">
+            <div class="p-8 rounded-full bg-cyan-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-qrcode" width="46" height="46" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M4 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z"></path>
+                    <path d="M7 17l0 .01"></path>
+                    <path d="M14 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z"></path>
+                    <path d="M7 7l0 .01"></path>
+                    <path d="M4 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z"></path>
+                    <path d="M17 7l0 .01"></path>
+                    <path d="M14 14l3 0"></path>
+                    <path d="M20 14l0 .01"></path>
+                    <path d="M14 14l0 3"></path>
+                    <path d="M14 20l3 0"></path>
+                    <path d="M17 17l3 0"></path>
+                    <path d="M20 17l0 3"></path>
+                </svg>
+            </div>
+            <h1 class="font-bold text-2xl border-b-4 border-[#ff948f] leading-tight rounded-full mb-2">4.</h1>
+            <h1>Vyber si doplnky ku pošte ako QR kód na video, voňavý papier a unikátne darčeky.</h1>
         </div>
-        <div class="w-1/5 flex flex-col place-items-center">
-            <div class="w-14 h-14 rounded-full bg-cyan-400"></div>
-            <h1>Vyber si doplnky ku pošte</h1>
-        </div>
-        <div class="w-1/5 flex flex-col place-items-center">
-            <div class="w-14 h-14 rounded-full bg-cyan-400"></div>
-            <h1>Zaplať a odošli</h1>
+        <div class="w-1/5 flex flex-col place-items-center border-b-2 border-r-2 border-gray-500 py-4 px-3 rounded-lg">
+            <div class="p-8 rounded-full bg-cyan-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-link" width="46" height="46" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M20.085 11.085l-8.085 -8.085l-9 9h2v7a2 2 0 0 0 2 2h4.5"></path>
+                    <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 1.807 1.143"></path>
+                    <path d="M21 21m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                    <path d="M21 16m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                    <path d="M16 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+                    <path d="M21 16l-5 3l5 2"></path>
+                </svg>
+            </div>
+            <h1 class="font-bold text-2xl border-b-4 border-[#ff948f] leading-tight rounded-full mb-2">5.</h1>
+            <h1>Vyplň adresné údaje a objednaj, o zvyšok sa postaráme my.</h1>
         </div>
 
     </div>
