9bfea23eaf58920622511e25f9cea8e8d1893e2a
ViktorHa96 <119804202+ViktorHa96@users.noreply.github.com>
2023-10-09 17:43:49 +0000

    pridanie gift obrazkov, podstránka pre gift page

--- resources/js/Pages/Menu/Gift/GiftPage.vue
+++ resources/js/Pages/Menu/Gift/GiftPage.vue
@@ -1,8 +1,72 @@
 <template>
-    <Head title="Galéria"/>
+    <Head title="Darčeky"/>
     <AppLayout>
-        <div class="w-full flex flex-col">
+        <div class="w-full flex flex-col mb-10 px-10">
+            <h2 class="specify-font uppercase text-2xl mt-16 mb-20 font-bold text-center" >pridajte darčeky a doplnky k vaším blahoželaniam</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 text-lg font-bold specify-font">
+                <div>
+                    <div class="mx-10 flex flex-col place-items-center gap-4">
+                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/cupon.jpeg" alt="">
+                        <h2 class="uppercase">Zľavové Kupóny</h2>
+                    </div>
+                </div>
 
+                <div>
+                    <div class="mx-10 flex flex-col place-items-center gap-4">
+                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/seeds.jpeg" alt="">
+                        <h2 class="uppercase">Magické Semienka</h2>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="mx-10 flex flex-col place-items-center gap-4">
+                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/tea1.jpeg" alt="">
+                        <h2 class="uppercase">Čaje z rôznych častí sveta</h2>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="mx-10 flex flex-col place-items-center gap-4">
+                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/sweets.jpeg" alt="">
+                        <h2 class="uppercase">Mini sladkosti</h2>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="mx-10 flex flex-col place-items-center gap-4">
+                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/necklaces.jpeg" alt="">
+                        <h2 class="uppercase">Náramky a náhrdelníky</h2>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="mx-10 flex flex-col place-items-center gap-4">
+                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/tea1.jpeg" alt="">
+                        <h2 class="uppercase">Osobné magnetky</h2>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="mx-10 flex flex-col place-items-center gap-4">
+                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/tea1.jpeg" alt="">
+                        <h2 class="uppercase">Vlastné QR kódy</h2>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="mx-10 flex flex-col place-items-center gap-4">
+                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/tea1.jpeg" alt="">
+                        <h2 class="uppercase">Parfúm na papier</h2>
+                    </div>
+                </div>
+
+                <div>
+                    <div class="mx-10 flex flex-col place-items-center gap-4">
+                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/tea1.jpeg" alt="">
+                        <h2 class="uppercase">Zvukový modul</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     </AppLayout>
 </template>
