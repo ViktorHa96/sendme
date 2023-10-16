1f4ced3f3bb35a096fa4d7058f0d42f039797f21
ViktorHa96 <119804202+ViktorHa96@users.noreply.github.com>
2023-10-16 12:40:37 +0000

    pridane masonry wall, upravenie blogu ukazovania a darčeky ukazovania

--- resources/js/Pages/Menu/Gift/GiftPage.vue
+++ resources/js/Pages/Menu/Gift/GiftPage.vue
@@ -2,69 +2,106 @@
     <Head title="Darčeky"/>
     <AppLayout>
         <div class="w-full flex flex-col mb-10 px-10">
-            <h2 class="specify-font uppercase text-2xl mt-16 mb-20 font-bold text-center" >pridajte darčeky a doplnky k vaším blahoželaniam</h2>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 text-lg font-bold specify-font">
+            <h2 class="flex w-fit mx-auto specify-font uppercase text-2xl mt-10 mb-10 font-bold text-center px-8 py-2 bg-cyan-100 rounded-full border-2 border-black" >pridajte darčeky a doplnky k vaším blahoželaniam</h2>
+            <h2 class="uppercase text-2xl font-bold mb-6  text-center" >Vyberte kategóriu</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10 text-sm font-bold specify-font text-center">
                 <div>
-                    <div class="mx-10 flex flex-col place-items-center gap-4">
-                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/cupon.jpeg" alt="">
+                    <Link href="/" class="mx-10 flex flex-col place-items-center gap-4 group">
+                        <div class="relative rounded-full overflow-hidden border-4 border-black/60">
+                            <div class="hidden bg-black/60 rounded-md absolute z-20 top-0 left-0 w-full h-full group-hover:flex justify-center"></div>
+                            <img class="rounded-full group-hover:scale-105 duration-150" src="../../../images/Gifts/cupon.jpeg" alt="">
+                            <h2 class="hidden center-div z-30 top-0 left-0 text-white text-2xl font-bold group-hover:flex justify-center place-items-center">Zobraziť</h2>
+                        </div>
                         <h2 class="uppercase">Zľavové Kupóny</h2>
-                    </div>
+                    </Link>
                 </div>
 
                 <div>
-                    <div class="mx-10 flex flex-col place-items-center gap-4">
-                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/seeds.jpeg" alt="">
+                    <Link href="/" class="mx-10 flex flex-col place-items-center gap-4 group">
+                        <div class="relative rounded-full overflow-hidden border-4 border-black/60">
+                            <div class="hidden bg-black/60 rounded-md absolute z-20 top-0 left-0 w-full h-full group-hover:flex justify-center"></div>
+                            <img class="rounded-full group-hover:scale-105 duration-150" src="../../../images/Gifts/seeds.jpeg" alt="">
+                            <h2 class="hidden center-div z-30 top-0 left-0 text-white text-2xl font-bold group-hover:flex justify-center place-items-center">Zobraziť</h2>
+                        </div>
                         <h2 class="uppercase">Magické Semienka</h2>
-                    </div>
+                    </Link>
                 </div>
 
                 <div>
-                    <div class="mx-10 flex flex-col place-items-center gap-4">
-                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/tea1.jpeg" alt="">
+                    <Link href="/" class="mx-10 flex flex-col place-items-center gap-4 group">
+                        <div class="relative rounded-full overflow-hidden border-4 border-black/60">
+                            <div class="hidden bg-black/60 rounded-md absolute z-20 top-0 left-0 w-full h-full group-hover:flex justify-center"></div>
+                            <img class="rounded-full group-hover:scale-105 duration-150" src="../../../images/Gifts/tea1.jpeg" alt="">
+                            <h2 class="hidden center-div z-30 top-0 left-0 text-white text-2xl font-bold group-hover:flex justify-center place-items-center">Zobraziť</h2>
+                        </div>
                         <h2 class="uppercase">Čaje z rôznych častí sveta</h2>
-                    </div>
+                    </Link>
                 </div>
 
                 <div>
-                    <div class="mx-10 flex flex-col place-items-center gap-4">
-                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/sweets.jpeg" alt="">
+                    <Link href="/" class="mx-10 flex flex-col place-items-center gap-4 group">
+                        <div class="relative rounded-full overflow-hidden border-4 border-black/60">
+                            <div class="hidden bg-black/60 rounded-md absolute z-20 top-0 left-0 w-full h-full group-hover:flex justify-center"></div>
+                            <img class="rounded-full group-hover:scale-105 duration-150" src="../../../images/Gifts/sweets.jpeg" alt="">
+                            <h2 class="hidden center-div z-30 top-0 left-0 text-white text-2xl font-bold group-hover:flex justify-center place-items-center">Zobraziť</h2>
+                        </div>
                         <h2 class="uppercase">Mini sladkosti</h2>
-                    </div>
+                    </Link>
                 </div>
 
                 <div>
-                    <div class="mx-10 flex flex-col place-items-center gap-4">
-                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/necklaces.jpeg" alt="">
+                    <Link href="/" class="mx-10 flex flex-col place-items-center gap-4 group">
+                        <div class="relative rounded-full overflow-hidden border-4 border-black/60">
+                            <div class="hidden bg-black/60 rounded-md absolute z-20 top-0 left-0 w-full h-full group-hover:flex justify-center"></div>
+                            <img class="rounded-full group-hover:scale-105 duration-150" src="../../../images/Gifts/necklaces.jpeg" alt="">
+                            <h2 class="hidden center-div z-30 top-0 left-0 text-white text-2xl font-bold group-hover:flex justify-center place-items-center">Zobraziť</h2>
+                        </div>
                         <h2 class="uppercase">Náramky a náhrdelníky</h2>
-                    </div>
+                    </Link>
                 </div>
 
                 <div>
-                    <div class="mx-10 flex flex-col place-items-center gap-4">
-                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/tea1.jpeg" alt="">
+                    <Link href="/" class="mx-10 flex flex-col place-items-center gap-4 group">
+                        <div class="relative rounded-full overflow-hidden border-4 border-black/60">
+                            <div class="hidden bg-black/60 rounded-md absolute z-20 top-0 left-0 w-full h-full group-hover:flex justify-center"></div>
+                            <img class="rounded-full group-hover:scale-105 duration-150" src="../../../images/Gifts/tea2.jpeg" alt="">
+                            <h2 class="hidden center-div z-30 top-0 left-0 text-white text-2xl font-bold group-hover:flex justify-center place-items-center">Zobraziť</h2>
+                        </div>
                         <h2 class="uppercase">Osobné magnetky</h2>
-                    </div>
+                    </Link>
                 </div>
 
                 <div>
-                    <div class="mx-10 flex flex-col place-items-center gap-4">
-                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/tea1.jpeg" alt="">
+                    <Link href="/" class="mx-10 flex flex-col place-items-center gap-4 group">
+                        <div class="relative rounded-full overflow-hidden border-4 border-black/60">
+                            <div class="hidden bg-black/60 rounded-md absolute z-20 top-0 left-0 w-full h-full group-hover:flex justify-center"></div>
+                            <img class="rounded-full group-hover:scale-105 duration-150" src="../../../images/Gifts/tea2.jpeg" alt="">
+                            <h2 class="hidden center-div z-30 top-0 left-0 text-white text-2xl font-bold group-hover:flex justify-center place-items-center">Zobraziť</h2>
+                        </div>
                         <h2 class="uppercase">Vlastné QR kódy</h2>
-                    </div>
+                    </Link>
                 </div>
 
                 <div>
-                    <div class="mx-10 flex flex-col place-items-center gap-4">
-                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/tea1.jpeg" alt="">
+                    <Link href="/" class="mx-10 flex flex-col place-items-center gap-4 group">
+                        <div class="relative rounded-full overflow-hidden border-4 border-black/60">
+                            <div class="hidden bg-black/60 rounded-md absolute z-20 top-0 left-0 w-full h-full group-hover:flex justify-center"></div>
+                            <img class="rounded-full group-hover:scale-105 duration-150" src="../../../images/Gifts/tea2.jpeg" alt="">
+                            <h2 class="hidden center-div z-30 top-0 left-0 text-white text-2xl font-bold group-hover:flex justify-center place-items-center">Zobraziť</h2>
+                        </div>
                         <h2 class="uppercase">Parfúm na papier</h2>
-                    </div>
+                    </Link>
                 </div>
 
                 <div>
-                    <div class="mx-10 flex flex-col place-items-center gap-4">
-                        <img class="rounded-full hover:scale-105 duration-150 border-4 border-black/60" src="../../../images/Gifts/tea1.jpeg" alt="">
+                    <Link href="/" class="mx-10 flex flex-col place-items-center gap-4 group">
+                        <div class="relative rounded-full overflow-hidden border-4 border-black/50">
+                            <div class="hidden bg-black/60 rounded-md absolute z-20 top-0 left-0 w-full h-full group-hover:flex justify-center"></div>
+                            <img class="rounded-full group-hover:scale-105 duration-150" src="../../../images/Gifts/tea2.jpeg" alt="">
+                            <h2 class="hidden center-div z-30 top-0 left-0 text-white text-2xl font-bold group-hover:flex justify-center place-items-center">Zobraziť</h2>
+                        </div>
                         <h2 class="uppercase">Zvukový modul</h2>
-                    </div>
+                    </Link>
                 </div>
             </div>
         </div>
